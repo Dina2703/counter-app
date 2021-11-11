8975265d5eaba5a53ce6354ca0fbb1e9e392bd71
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-11-10 20:46:54 -0500

    conditional rendering

--- src/components/counter.js
+++ src/components/counter.js
@@ -4,17 +4,28 @@ import React, { Component } from 'react';
 class Counter extends React.Component {
     state = {
         count: 0,
-        imageUrl: 'https:picsum.photos/200'
+        imageUrl: 'https:picsum.photos/200',
+        tags: ['tag1', 'tag2', 'tag3']
     };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return <ul>{ this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>;
+    }
 
     render() { 
 
 
         return (
             <div className="m-4" >
-                <img src={ this.state.imageUrl }/>
+                <img src={ this.state.imageUrl } alt="random pic"/>
                 <span style= {{fontSize: 20}} className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
-                <button className="btn btn-secondary" >CLICK</button>
+                <button className="btn btn-secondary" >add</button>
+                <div>
+                    {this.state.tags.length === 0 && "Please create a new tag!" || "my list"}
+                    { this.renderTags()}
+                </div>
             </div>
             );
     }
