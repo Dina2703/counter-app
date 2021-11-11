f3cdd234d88e0a89e4b0f32cda00a1f701c02125
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-11-10 20:05:05 -0500

    adding class and style-1

--- src/components/counter.js
+++ src/components/counter.js
@@ -3,19 +3,26 @@ import React, { Component } from 'react';
 
 class Counter extends React.Component {
     state = {
-        count: 0
+        count: 0,
+        imageUrl: 'https:picsum.photos/200'
     };
+    styles = {
+        fontSize: 20,
+        fontWeight: "bold"
+    };
+
     render() { 
         return (
-            <div>
-                <span>{ this.formatCount() }</span>
-                <button >CLICK</button>
+            <div className="m-4" >
+                <img src={ this.state.imageUrl }/>
+                <span style= {this.styles} className="badge badge-primary m-3">{ this.formatCount() }</span>
+                <button className="btn btn-secondary" >CLICK</button>
             </div>
             );
     }
     formatCount(){
         const { count } = this.state;
-        return count === 0 ? <h1>Zero</h1> : count;
+        return count === 0 ? "Zero" : count;
     }
 }
 
