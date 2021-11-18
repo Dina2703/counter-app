38ae0f198b6a29e35a51e64bd2d52515c4696a09
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-11-18 10:19:50 -0500

    testing

---
+++ src/components/test.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Test extends React.Component {
+  render() { 
+    return <div>
+        <h2> Testing </h2>
+    </div>;
+  }
+}
+ 
+export default Test;
